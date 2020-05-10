f2e49a64d96e89bae09dd64a2bc9a5e3f473bb76
Zavodil <support@zavodil.ru>
2020-05-10 23:38:43 +0300

    ViewAccessKey command added

--- Commands/ViewAccessKeyCommand.php
+++ Commands/ViewAccessKeyCommand.php
@@ -8,7 +8,7 @@ use Near\NearData;
 
 include_once __DIR__ . '/../near.php';
 
-class ViewAccountCommand extends UserCommand
+class ViewAccessKeyCommand extends UserCommand
 {
     public function execute()
     {
@@ -19,24 +19,22 @@ class ViewAccountCommand extends UserCommand
         if($paramPosition > -1) {
             $account = substr($message->text, $paramPosition + 1);
             if($account) {
-                $accountData = NearData::GetAccountBalance($account);
+                $accountData = NearData::GetNearRpcData("query", ["request_type" => "view_access_key_list", "finality" => "final", "account_id" => $account]);
 
                 if(isset($accountData["error"]))
                     $reply = $accountData["error"]["message"]." ".$accountData["error"]["data"];
                 else {
-                    $output = [
-                        "Account " . $account,
-                        "Balance: " . NearData::RoundNearBalance($accountData["result"]["amount"]),
-                        "Locked: " . NearData::RoundNearBalance($accountData["result"]["locked"]),
-                        "Storage Usage: " . NearData::RoundNearBalance($accountData["result"]["storage_usage"]),
-                        "Access Keys List: /ViewAccessKey ".$account
-                    ];
+                    $output[] = "Account " . $account;
+                    foreach($accountData["result"]["keys"] as $key){
+                        $output[] = "- ".$key["public_key"]." (".$key["access_key"]["permission"].", nonce: ".$key["access_key"]["nonce"].")";
+                    }
+
                     $reply = join(chr(10), $output);
                 }
             }
         }
         else
-            $reply = "Account now found. Usage: /ViewAccount username".$paramPosition;
+            $reply = "Account now found. Usage: /ViewAccessKey username".$paramPosition;
 
         $data = [
             'chat_id' => $chat_id,
